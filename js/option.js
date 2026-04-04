1e146cde4cde0b1580e66ea6087df15b540cad01
koimo koimo <koimokoimo@koimonoMacBook-Air.local>
2026-04-04 19:27:59 +0900

    Enable mobile scaling

--- js/option.js
+++ js/option.js
@@ -2,6 +2,7 @@
 if (! ("optionProps" in window)) {
 	window.optionProps = {};
 }
-window.optionProps.magnify = false;
+window.optionProps.magnify = true;
+
 window.optionProps.hasInstanceStorage = false;
 	
